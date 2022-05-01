d59192d7460a4068d8c63a43b6f6ba6c411737c5
Emre Dogan <emredogan@outlook.com>
2022-05-01 23:40:22 +0200

    Refactoring code

--- Football_API/Controller/ViewController.swift
+++ Football_API/Controller/ViewController.swift
@@ -10,8 +10,6 @@ import Kingfisher
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-
-
 class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     let networkingClient = NetworkService()
@@ -19,82 +17,41 @@ class ViewController: UIViewController {
     private var teams = [Team]()
     private let fireDB = Firestore.firestore()
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.tableFooterView = UIView()   // To hide the empty cells
         tableView.dataSource = self
         tableView.delegate = self
-        
-        
-        // Do any additional setup after loading the view.
+        tableView.tableFooterView = UIView()   // To hide the empty cells
+        downloadData()
+    }
+    
+    func downloadData() {
         networkingClient.makeDataRequest(serviceName: .AF) { result in
             switch result {
             case .success(let teams):
                 self.handleResult(result: teams)
             case .failure(let error):
-                
+                print(error)
+                // REGULAR API CALL HAS FAILED ATTEMPTING TO GET DATA FROM THE FIRESTORE
                 self.networkingClient.getDataFromFirebase { result in
                     switch result {
                     case .success(let teams):
                         self.handleResult(result: teams)
-                    case .failure(let error): break
-                        
-                        
-                        
-                        
+                    case .failure(let error):
+                        print(error)
                     }
                 }
-                
-                
-                
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
-                
-                
             }
-            
         }
     }
     
     func handleResult(result: [Team]) {
         self.teams = result
         teams.sort(by: {$0.name < $1.name})
-        
         DispatchQueue.main.async {
             self.tableView.reloadData()
         }
-        
     }
-    
-    
-    
-    // USED TO TEMPORARLY SAVE DATA IN FIREBASE STORE
-    /*func writeData(teams: [Team]) {
-     print("WRITING DATA TO FIREBASE")
-     
-     for team in teams {
-     do { // 2
-     let team = team
-     
-     try fireDB.collection("myTeams").document(randomString(length: 10)).setData(from: team)
-     } catch {
-     // handle the error here
-     print("ERROR is saving to DB")
-     }
-     }
-     
-     
-     
-     }
-     
-     func randomString(length: Int) -> String {
-     let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-     return String((0..<length).map{ _ in letters.randomElement()! })
-     } */
 }
 
 extension ViewController : UITableViewDelegate, UITableViewDataSource {
