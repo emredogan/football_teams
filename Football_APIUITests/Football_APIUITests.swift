11d95dc7d6852786a4e08ad9fc5ccb0c97923536
Emre Dogan <emredogan@outlook.com>
2022-05-07 12:04:53 +0200

    Added UI testing

---
+++ Football_APIUITests/Football_APIUITests.swift
@@ -0,0 +1,38 @@
+//
+//  Football_APIUITests.swift
+//  Football_APIUITests
+//
+//  Created by Emre Dogan on 07/05/2022.
+//
+
+import XCTest
+
+class Football_APIUITests: XCTestCase {
+    let app = XCUIApplication()
+
+    func testSuccessfulLogin() {
+        app.launch()
+        let eMailTextField = app.textFields["E-mail"]
+        eMailTextField.tap()
+        eMailTextField.typeText("EMRE DOGAN")
+        dismissKeyboardIfPresent()
+        
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("123456")
+        dismissKeyboardIfPresent()
+
+        app.staticTexts["LOGIN"].tap()
+        app.staticTexts["Premier League"].tap()
+    }
+    
+    func dismissKeyboardIfPresent() {
+        if app.keyboards.element(boundBy: 0).exists {
+            if UIDevice.current.userInterfaceIdiom == .pad {
+                app.keyboards.buttons["Hide keyboard"].tap()
+            } else {
+                app.toolbars.buttons["Done"].tap()
+            }
+        }
+    }
+}
