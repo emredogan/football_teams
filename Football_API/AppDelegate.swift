726b8cec76efe42287bab25a9440d46d05e6b62b
Emre Dogan <emredogan@outlook.com>
2022-05-17 01:19:21 +0200

    Refactor AppDelegate

--- Football_API/AppDelegate.swift
+++ Football_API/AppDelegate.swift
@@ -13,23 +13,18 @@ import IQKeyboardManager
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Library setup
+        IQKeyboardManager.shared().isEnabled = true
         FirebaseApp.configure()
         
+        // Delegate
         Messaging.messaging().delegate = self
         UNUserNotificationCenter.current().delegate = self
-        
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, _ in
-            guard success else {
-                return
-            }
-            print("Success")
-        }
-        
-        application.registerForRemoteNotifications()
-        IQKeyboardManager.shared().isEnabled = true
+
+        requestNotificationAccess(application)
         return true
     }
     
@@ -39,24 +34,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUser
             guard let token = token else {
                 return
             }
-            
             print("Token \(token)")
-
         }
     }
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func requestNotificationAccess(_ application: UIApplication) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, _ in
+            guard success else {
+                return
+            }
+            print("Success for notification permission")
+            application.registerForRemoteNotifications()
+        }
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
